d467e3ef98db6064b0fc750dba825a2cf36348ea
Arkadiusz Mika <arekmika4@gmail.com>
2023-06-13 09:24:16 +0200

    Create printWeatherData function

---
+++ include/printWeatherData.h
@@ -0,0 +1,8 @@
+#ifndef WEATHER_TRACKER_PRINTWEATHERDATA_H
+#define WEATHER_TRACKER_PRINTWEATHERDATA_H
+
+void printWeatherData(const std::string &cityName, double temperatureInCelsius, const std::string &weatherDescription,
+                      int humidity);
+
+
+#endif //WEATHER_TRACKER_PRINTWEATHERDATA_H
---
+++ src/printWeatherData.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include "../include/printWeatherData.h"
+
+void printWeatherData(const std::string &cityName, double temperatureInCelsius, const std::string &weatherDescription,
+                      int humidity) {
+
+    std::cout << std::fixed << std::setprecision(1);
+
+    std::cout << "Weather for city " << cityName << ":" << std::endl;
+    std::cout << "Temperature: " << temperatureInCelsius << " C" << std::endl;
+    std::cout << "Weather description: " << weatherDescription << std::endl;
+    std::cout << "Humidity: " << humidity << "%" << std::endl;
+}
